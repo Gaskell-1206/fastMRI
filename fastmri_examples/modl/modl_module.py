2b71f044f7e34e18f36e243507fc3c4ea57589fb
Shengjia Chen <gaskell1206@hotmail.com>
2025-08-15 11:29:30 -0400

    Integrate MoDL model with fastMRI pipeline and add training script

---
+++ fastmri_examples/modl/modl_module.py
@@ -0,0 +1,81 @@
+
+import torch
+import torch.nn as nn
+import pytorch_lightning as pl
+from fastmri.data import transforms as fastmri_transforms
+from fastmri.pl_modules.mri_module import MriModule
+from fastmri.models.modl import MoDL
+from fastmri.models.varnet import SensitivityModel
+
+class MoDLModule(MriModule):
+    """
+    PyTorch Lightning module for MoDL model using fastMRI data pipeline.
+    """
+    def __init__(
+        self,
+        n_layers: int = 5,
+        k_iters: int = 10,
+        lr: float = 1e-3,
+        weight_decay: float = 0.0,
+        sens_chans: int = 8,
+        sens_pools: int = 4,
+        **kwargs
+    ):
+        super().__init__(None, None, lr, weight_decay, **kwargs)
+        self.model = MoDL(n_layers=n_layers, k_iters=k_iters)
+        self.sens_net = SensitivityModel(
+            chans=sens_chans,
+            num_pools=sens_pools,
+            in_chans=2,
+            out_chans=2,
+            drop_prob=0.0,
+            mask_center=True,
+        )
+        
+        self.loss = fastmri.SSIMLoss()
+
+    @staticmethod
+    def add_model_specific_args(parent_parser):
+        parser = parent_parser.add_argument_group("MoDL")
+        parser.add_argument("--n_layers", default=5, type=int, help="Number of CNN layers in MoDL denoiser")
+        parser.add_argument("--k_iters", default=10, type=int, help="Number of MoDL iterations")
+        parser.add_argument("--lr", default=1e-3, type=float, help="Learning rate")
+        parser.add_argument("--weight_decay", default=0.0, type=float, help="Weight decay")
+        parser.add_argument("--sens_chans", default=8, type=int, help="Sensitivity map U-Net channels")
+        parser.add_argument("--sens_pools", default=4, type=int, help="Sensitivity map U-Net pools")
+        return parent_parser
+
+    def forward(self, image, csm, mask):
+        return self.model(image, csm, mask)
+
+    def _prepare_inputs(self, batch):
+        masked_kspace, mask, target, attrs, fname, slice_num = batch
+        csm = self.sens_net(masked_kspace, mask)
+        image = fastmri_transforms.ifft2c(masked_kspace)
+        image = fastmri_transforms.complex_abs(image)
+        image = fastmri_transforms.complex_center_crop(image, (image.shape[-2], image.shape[-1]))
+        image = fastmri_transforms.to_tensor(image)
+        mask = mask.squeeze(1)
+        target = fastmri_transforms.to_tensor(target)
+        return image, csm, mask, target
+
+    def training_step(self, batch, batch_idx):
+        image, csm, mask, target = self._prepare_inputs(batch)
+        output = self(image, csm, mask)
+        loss = self.loss(output, target)
+        self.log("train_loss", loss)
+        return loss
+
+    def validation_step(self, batch, batch_idx):
+        image, csm, mask, target = self._prepare_inputs(batch)
+        output = self(image, csm, mask)
+        val_loss = self.loss(output, target)
+        self.log("val_loss", val_loss, prog_bar=True)
+        return val_loss
+
+    def test_step(self, batch, batch_idx):
+        image, csm, mask, target = self._prepare_inputs(batch)
+        output = self(image, csm, mask)
+        test_loss = self.loss(output, target)
+        self.log("test_loss", test_loss)
+        return test_loss
---
+++ fastmri_examples/modl/train_modl_demo.py
@@ -0,0 +1,150 @@
+"""
+Demo script for training/testing MoDL model using fastMRI data pipeline.
+"""
+import os
+import pathlib
+from argparse import ArgumentParser
+import pytorch_lightning as pl
+from fastmri.data.subsample import create_mask_for_mask_type
+from fastmri.data.transforms import VarNetDataTransform
+from fastmri.pl_modules import FastMriDataModule
+from modl_module import MoDLModule
+
+def cli_main(args):
+    pl.seed_everything(args.seed)
+
+    # Data transforms
+    mask = create_mask_for_mask_type(
+        args.mask_type, args.center_fractions, args.accelerations
+    )
+    train_transform = VarNetDataTransform(mask_func=mask, use_seed=False)
+    val_transform = VarNetDataTransform(mask_func=mask)
+    test_transform = VarNetDataTransform()
+
+    data_module = FastMriDataModule(
+        data_path=args.data_path,
+        challenge=args.challenge,
+        train_transform=train_transform,
+        val_transform=val_transform,
+        test_transform=test_transform,
+        test_split=args.test_split,
+        test_path=args.test_path,
+        sample_rate=args.sample_rate,
+        batch_size=args.batch_size,
+        num_workers=args.num_workers,
+        distributed_sampler=(args.accelerator in ("ddp", "ddp_cpu")),
+    )
+
+    # Model
+    model = MoDLModule(
+        n_layers=args.n_layers,
+        k_iters=args.k_iters,
+        lr=args.lr,
+        weight_decay=args.weight_decay,
+    )
+
+    # Trainer
+    trainer = pl.Trainer.from_argparse_args(args)
+
+    # Run
+    if args.mode == "train":
+        trainer.fit(model, datamodule=data_module)
+    elif args.mode == "test":
+        trainer.test(model, datamodule=data_module)
+    else:
+        raise ValueError(f"unrecognized mode {args.mode}")
+
+def build_args():
+    parser = ArgumentParser()
+    path_config = pathlib.Path("../../fastmri_dirs.yaml")
+    backend = "ddp"
+    num_gpus = 2 if backend == "ddp" else 1
+    batch_size = 1
+    from fastmri.data.mri_data import fetch_dir
+    data_path = fetch_dir("knee_path", path_config)
+    default_root_dir = fetch_dir("log_path", path_config) / "modl" / "modl_demo"
+
+    parser.add_argument(
+        "--mode",
+        default="train",
+        choices=("train", "test"),
+        type=str,
+        help="Operation mode",
+    )
+    parser.add_argument(
+        "--mask_type",
+        choices=("random", "equispaced_fraction"),
+        default="equispaced_fraction",
+        type=str,
+        help="Type of k-space mask",
+    )
+    parser.add_argument(
+        "--center_fractions",
+        nargs="+",
+        default=[0.08],
+        type=float,
+        help="Number of center lines to use in mask",
+    )
+    parser.add_argument(
+        "--accelerations",
+        nargs="+",
+        default=[4],
+        type=int,
+        help="Acceleration rates to use for masks",
+    )
+
+    parser = FastMriDataModule.add_data_specific_args(parser)
+    parser.set_defaults(
+        data_path=data_path,
+        mask_type="equispaced_fraction",
+        challenge="multicoil",
+        batch_size=batch_size,
+        test_path=None,
+    )
+
+    parser = MoDLModule.add_model_specific_args(parser)
+    parser.set_defaults(
+        n_layers=5,
+        k_iters=10,
+        lr=1e-3,
+        weight_decay=0.0,
+    )
+
+    parser = pl.Trainer.add_argparse_args(parser)
+    parser.set_defaults(
+        gpus=num_gpus,
+        replace_sampler_ddp=False,
+        strategy=backend,
+        seed=42,
+        deterministic=True,
+        default_root_dir=default_root_dir,
+        max_epochs=50,
+    )
+
+    args = parser.parse_args()
+
+    # Checkpointing
+    checkpoint_dir = args.default_root_dir / "checkpoints"
+    if not checkpoint_dir.exists():
+        checkpoint_dir.mkdir(parents=True)
+    args.callbacks = [
+        pl.callbacks.ModelCheckpoint(
+            dirpath=args.default_root_dir / "checkpoints",
+            save_top_k=True,
+            verbose=True,
+            monitor="val_loss",
+            mode="min",
+        )
+    ]
+    if getattr(args, "resume_from_checkpoint", None) is None:
+        ckpt_list = sorted(checkpoint_dir.glob("*.ckpt"), key=os.path.getmtime)
+        if ckpt_list:
+            args.resume_from_checkpoint = str(ckpt_list[-1])
+    return args
+
+def run_cli():
+    args = build_args()
+    cli_main(args)
+
+if __name__ == "__main__":
+    run_cli()
